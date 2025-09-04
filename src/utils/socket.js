270d1705386ce3cf94619c910c455c60bea55183
Chiranjeev Singh Sethi <chiranjeev@Chiranjeevs-MacBook-Air.local>
2025-09-04 19:05:19 +0530

    vercel deploy commit_3

--- src/utils/socket.js
+++ src/utils/socket.js
@@ -2,7 +2,7 @@ import {io} from 'socket.io-client'
 import {BASE_URL} from './constant'
 export const createSocketConnection=()=>{
 console.log("JWT Token:", localStorage.getItem("token"))
-const socket=io(BASE_URL,{
+const socket=io(import.meta.env.SOCKET_IO_URL,{
 auth:{
 token:localStorage.getItem("token")
 },
